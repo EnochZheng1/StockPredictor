4b635897fb22ec4b3fea46c23df1e0e2de8faf80
EnochZheng <enochzheng1109@gmail.com>
2024-02-28 19:15:37 -0600

    add weekday function

--- backend/utils/data_Analysis.py
+++ backend/utils/data_Analysis.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 import talib
+from datetime import datetime
+
+def weekday(df):
+    df["Wday"] = df['Date'].dt.weekday + 1
+    return df
 
 def simple_moving_average(df, timeInterval=20):
     df["SMA_{}".format(timeInterval)] = df['Adj Close'].rolling(window=timeInterval).mean()
@@ -98,6 +103,8 @@ def williams_r(df, timeInterval=14):
     return df
     
 def technical_indictors_calculation(df):
+    weekday(df)
+    
     # Calculate the 20-day Simple Moving Average (SMA)
     simple_moving_average(df)
     
